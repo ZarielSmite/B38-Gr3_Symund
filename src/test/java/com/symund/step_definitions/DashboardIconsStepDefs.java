6b7c5142100b9f00633a27664a0f11bab27289c1
Muzaffaryuldoshev <yuldoshevmuzaffarr@gmail.com>
2025-02-24 17:57:45 +0100

    Some steps are implemented and ready to function properly

--- src/test/java/com/symund/step_definitions/DashboardIconsStepDefs.java
+++ src/test/java/com/symund/step_definitions/DashboardIconsStepDefs.java
@@ -10,16 +10,30 @@ public class DashboardIconsStepDefs {
 
     DashboardIconsPage dashboardPage = new DashboardIconsPage();
 
+    @Then("user should navigate to {string} btn")
+    public void userShouldNavigateToBtn(String magnifyBtn) {
+        dashboardPage.magnifyIcon.click();
+
+    }
+
 
     @Given("user types file name inside any Module")
     public void userTypesFileNameInsideAnyModule() {
+       dashboardPage.searchFile("RealAgile");
+
     }
 
     @When("user clicks on that file")
     public void userClicksOnThatFile() {
+        dashboardPage.clickToResult("RealAgile");
+
     }
 
     @Then("verify that user can see detailed side page of file")
     public void verifyThatUserCanSeeDetailedSidePageOfFile() {
+        dashboardPage.verifyDetailedSidePageOfFile();
+
     }
+
+
 }
